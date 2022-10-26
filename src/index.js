13b3c474cc1fcd41469987badcf311d5e0b8c6ad
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-10-26 14:21:10 +0300

    folder structures and routing added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { ChakraProvider, theme } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -5,6 +6,10 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
+
+export const server = `https://api.coingecko.com/api/v3`;
