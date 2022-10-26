a08ba2e21f1bd89022f994c48aea6ff44fd7bd08
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-10-26 14:49:22 +0300

    exchanges details added from api

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,7 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { server } from "../index";
+import Loader from "./Loader";
+import ErrorComponent from "./ErrorComponent";
+import {
+  Container,
+  Text,
+  Heading,
+  HStack,
+  VStack,
+  Image,
+} from "@chakra-ui/react";
 
 const Exchanges = () => {
-  return <div>Exchanges</div>;
+  const [exchanges, setExchanges] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchExchanges = async () => {
+      try {
+        const { data } = await axios.get(`${server}/exchanges`);
+        setExchanges(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchExchanges();
+  }, []);
+
+  if (error)
+    return <ErrorComponent message={"Error While Fetching Exchanges"} />;
+
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {" "}
+          <HStack wrap={"wrap"} justifyContent={"space-evenly"}>
+            {exchanges.map((i) => (
+              <ExchangeCard
+                key={i.id}
+                name={i.name}
+                img={i.image}
+                rank={i.trust_score_rank}
+                url={i.url}
+              />
+            ))}
+          </HStack>
+        </>
+      )}
+    </Container>
+  );
 };
+
+const ExchangeCard = ({ name, img, rank, url }) => (
+  <a href={url} target={"blank"}>
+    {/* //target blank means clicking multiple exchange links will use a single tab not opening a new tab 
+    for every click, _target opens new tab */}
+    <VStack
+      w={"52"}
+      shadow={"lg"}
+      p={"8"}
+      borderRadius={"lg"}
+      transition={"all 0.3s"}
+      m={"4"}
+      css={{
+        "&:hover": {
+          transform: "scale(1.1)",
+        },
+      }}
+    >
+      <Image
+        src={img}
+        w={"10"}
+        h={"10"}
+        objectFit={"contain"}
+        alt={"Exchange"}
+      />
+      <Heading size={"md"} noOfLines={1}>
+        {rank}
+      </Heading>
+
+      <Text noOfLines={1}>{name}</Text>
+    </VStack>
+  </a>
+);
 
 export default Exchanges;
